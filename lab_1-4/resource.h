cde739738f79c8b7f7a81031f8cadd422a1ab4f8
Obynochniy Alexandr <alexobyn655@gmail.com>
2023-11-22 17:53:36 +0200

    add 1-4 labs

---
+++ lab_1-4/resource.h
@@ -0,0 +1,68 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Painter.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_PAINTETYPE                  129
+#define IDD_PAGE_PROPERTY               130
+#define IDR_MAINFRAME1                  130
+#define IDB_PATTERN1                    131
+#define IDD_IMAGES                      132
+#define IDB_PATTERN2                    134
+#define IDB_PATTERN3                    135
+#define IDC_CURSOR_SQUARE               135
+#define IDB_PATTERN4                    136
+#define IDC_CURSOR_POLYGON              136
+#define IDB_PATTERN5                    137
+#define IDC_CURSOR_SURFACE              137
+#define IDB_PATTERN6                    138
+#define IDC_CURSOR_CIRCLE               138
+#define IDB_PATTERN7                    139
+#define IDB_PATTERN0                    140
+#define IDC_CURSOR_ELLIPSE              142
+#define IDC_CURSOR_STAR                 158
+#define IDC_CURSOR_SPLINE               159
+#define IDB_PATTERN8                    160
+#define IDC_CURSOR1                     161
+#define IDC_PICTURE                     161
+#define IDC_EDIT_WIDTH                  1001
+#define IDC_IMAGE_LIST                  1002
+#define IDC_EDIT_HEIGHT                 1003
+#define IDC_SPIN_WIDTH                  1004
+#define IDC_SPIN_HEIGHT                 1005
+#define IDC_STATIC_ICON                 1005
+#define ID_TEXT                         32771
+#define ID_FILE_PAGEPROPERTY            32772
+#define ID_EDIT_ADDSHAPE_POINT          32773
+#define ID_EDIT_ADDSHAPE_CIRCLE         32774
+#define ID_EDIT_ADDSHAPE_SQUARE         32775
+#define ID_EDIT_ADDSHAPE_LINE           32776
+#define ID_EDIT_SELECT                  32777
+#define ID_EDIT_ADDSHAPE_POLILINE       32778
+#define ID_EDIT_ADDSHAPE_POLIGON        32779
+#define ID_EDIT_CHANGEORDER_TOP         32780
+#define ID_EDIT_CHANGEORDER_STEPUP      32781
+#define ID_EDIT_CHANGEORDER_STEPDOWN    32782
+#define ID_EDIT_CHANGEORDER_BOTTOM      32783
+#define ID_EDIT_DELETE                  32784
+#define ID_EDIT_ADDSHAPE_POLYGON        32785
+#define ID_EDIT_ADDSHAPE_POLYLINE       32786
+#define ID_EDIT_ADDSHAPE_SURFACE        32787
+#define ID_SAVE_BMP                     32789
+#define ID_EDIT_ADDSHAPE_ELLIPSE        32790
+#define ID_EDIT_ADDSHAPE_STAR           32794
+#define ID_EDIT_ADDSHAPE_SPLINE         32797
+#define ID_EDIT_ADDSHAPE_PICTURE        32800
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        162
+#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
